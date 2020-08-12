c9ff8891be511644214e19b7f301cf8697a8c7f1
TshepoMakgtesi99 <tshepo@adadev.org.za>
2020-08-12 16:24:06 +0200

    Fixed my code was NYC

--- js/myscript.js
+++ js/myscript.js
@@ -10,6 +10,9 @@
 // Print the value of y
 
 
+// pseudocode
+// a notation resembling a simplified programming language, used in program design.
+
 // part 1
 
 let x = 0;
@@ -37,72 +40,71 @@ console.log(Y);
 
 
 
-let Resultx = 1 + 1 * 2;
-let Resulty = (1 + 1) * 2;
-let Resultz = 1 + ( 1 * 2 );
-let Resulta =  1 + 1 * 2 / 2;
-let Resultb =  (1 + 1 * 2 ) /  2;
+let ltx = 1 + 1 * 2;
+let lty = (1 + 1) * 2;
+let ltz = 1 + ( 1 * 2 );
+let lta =  1 + 1 * 2 / 2;
+let ltb =  (1 + 1 * 2 ) /  2;
 
-console.log(Resultx);
-console.log(Resulty);
-console.log(Resultz);
-console.log(Resulta);
-console.log(Resultb);
+console.log(ltx);
+console.log(lty);
+console.log(ltz);
+console.log(lta);
+console.log(ltb);
 
-// TASK 3 Question.........................................................................
-// Write a function that takes 2 numbers as input. If either of the numbers is 65, OR if the sum of the numbers is 65 then return true. Otherwise return false
 
-// Answer...............................................................................
 
-const always = 65;
-let numOne = 6;
-let numTwo = 45;
+// Write a function that takes 2 numbers as input. If either of the numbers is 65, OR if the sum of the numbers is 65 then return true. Otherwise return false.
+function numbers(x, y) 
+{
+  return ((x == 65 || y == 65) || (x + y == 65));
+}
+console.log(numbers(65, 65))
+console.log(numbers(30, 65))
+console.log(numbers(10, 10))
+console.log(numbers(30, 40))
 
-let sum = numOne + numTwo ;
-
-if (sum === always || numOne === always || numTwo === always){
-    console.log("true");
-}else{
-    console.log("false");
-};
 
 // TASK 4 Question...............................................................
 // Write a function that takes 2 numbers as input. If either of the numbers is 3 AND the sum of the numbers contains a 3 then return true. Otherwise return false
 
 // Answer...............................................................................
-const alwaysNow = 3;
-let InputOne = 3;
-let InputTwo = 0;
-
-let Newsum = InputOne + InputTwo ;
-
-if (InputOne === alwaysNow && Newsum === alwaysNow ){
-    console.log("true");
-}else if (InputTwo === alwaysNow && Newsum === alwaysNow ){
-    console.log("true");
-}else {
-    console.log("false");
-};
+function three(x, y) 
+{
+  return ((x == 3 || y == 3) || (x + y == 3));
+}
+console.log(three(3, 3))
+console.log(three(5, 3))
+console.log(three(15, 15))
+console.log(three(20, 30))
 
 // Test 5
 // Write a function that takes in three numbers. These numbers represent the lengths of the sides of a triangle. The function should return the area of a triangle.
-    let b = 4;
-    let h = 4;
+function TriangleArea(side1, side2, side3) {
 
-    let area = (b*h)/2;
-    console.log(area)
+    let s = (side1+side2+side3)/2;
+    let area = Math.sqrt(s*(s - side1)*(s-side2)*(s-side3));
+
+    console.log(area);
+
+}
+        TriangleArea(2,4,4);
 
     // TASK 6
     // White a function that takes in three numbers and returns the maximum number. Do this without using any builtin methods. Write your own logic from scratch.
 
-    const Newarray = [375,5000,150]
+        // Here we say return rather than console.log because we do not just want the value
+        // to be displayed we want to store it so that we can use it later.
+    // So now when we call this it won’t display on the console
 
-    Newarray.sort()
-    
-    Newarray.sort((a,b) => a-b)
-    
-    const highest = Newarray[Newarray.length-1]
-    console.log(highest);
+function max(input) {
+    if (toString.call(input) !== "[Largest Number]")  
+    return Math.max.apply(null, input);
+   }
+
+    console.log(max([10,23,65,3]));
+
+
     
 
 // Task 7
@@ -127,46 +129,47 @@ Ftshe(40);
  
 // Task 8
 // Make a function to convert any number into hours and minutes. (For example, 71 will become “1 hour, 11 minutes”; 133 will become “2 hours, 13 minutes”.)
+
 function timeConvertt(n) {
-    let num = 100;
-    let hours = (num / 60);
-    let Thours = Math.floor(hours);
-    let minutes = (hours - Thours) * 60;
-    let Tminutes = Math.round(minutes);
-    return num + " minutes = " + Thours + " hour(s) and " + Tminutes + " minute(s).";
+    let number = 100;
+    let hours = (number / 60);
+    let rehours = Math.floor(hours);
+    let minutes = (hours - rehours) * 60;
+    let remminutes = Math.round(minutes);
+    return number + " minutes = " + rehours + " hour(s) and " + remminutes + " minute(s).";
     }
     
     console.log(timeConvertt());
 
+
+
+
+
     // Task 9
     // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
         // Find the sum of all the multiples of 3 or 5 below 1000.
-
-    let natural  = function (n) {
         let sum = 0;
-        for (let i = 1; i < n; i++) {
-            if ((i % 3 == 0) || (i % 5 == 0)) {
-                sum += i;
+        for (let v = 0; v < 1000; v++)
+        {
+            if (v % 3 === 0 || v % 5 === 0)
+            {
+               sum += v;
             }
         }
-        document.write(sum);
-    };
+        console.log(sum);
+    // Task 10
+    // Write a function that takes in a string and then prints out all the vowels in the string. Make sure it can deal with capital letters and small letters.
+    let vowels = "aeiouAEIOU";
+    let string = "JavaScript Pretteir";
     
+    let array = "";
+    for (i = 0; i < string.length; i++){
+        if(vowels.indexOf(string[i]) >= 0) array += string[i];
     
-    natural (10);
+    }
+    console.log(array);
 
-// Task 10
-// Write a function that takes in a string and then prints out all the vowels in the string. Make sure it can deal with capital letters and small letters.
 
-let vowels = "aeiouAEIOU";
-let string = "JavaScript Pretteir";
-
-let array = "";
-for (i = 0; i < string.length; i++){
-    if(vowels.indexOf(string[i]) >= 0) array += string[i];
-
-}
-console.log(array);
 
 // Task 11
 // Make a function that takes two strings as input, and outputs the common characters/letters that they share. (For example, Input: ‘house’, ‘computers’ . Output: ‘Common letters: o, u, e, s’)
@@ -183,4 +186,5 @@ console.log(`Common letters: ${common}`);
 }
 
 find('Tshepo', 'Makgetsi');
+
 
