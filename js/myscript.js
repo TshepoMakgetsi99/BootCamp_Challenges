0faa2f089fb787b830890831cc650bd2810145c8
unknown <tshepomakgetsi@gmail.com>
2020-11-10 08:05:04 +0200

    fixed task 3 of pre bootcamp challenges

--- js/myscript.js
+++ js/myscript.js
@@ -55,14 +55,13 @@ console.log(ltb);
 
 
 // Write a function that takes 2 numbers as input. If either of the numbers is 65, OR if the sum of the numbers is 65 then return true. Otherwise return false.
-function numbers(x, y) 
+function numbers(x,y)
 {
-  return ((x == 65 || y == 65) || (x + y == 65));
+  return ((x == 65 || y == 65)||(x + y == 65));
 }
 console.log(numbers(65, 65))
-console.log(numbers(30, 65))
-console.log(numbers(10, 10))
-console.log(numbers(30, 40))
+console.log(numbers(20, 20))
+
 
 
 // TASK 4 Question...............................................................
