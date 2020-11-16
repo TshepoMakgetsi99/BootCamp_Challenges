149973dcd9f56a217d20e7608b9be106ebbadf01
unknown <tshepomakgetsi@gmail.com>
2020-11-16 09:36:56 +0200

    TaskThree

---
+++ js/myscript.js
@@ -0,0 +1,15 @@
+
+// Write a function that takes 2 numbers as input. If either of the numbers is 65, OR if the sum of the numbers is 65 then return true. Otherwise return false.
+function numbers(x,y){
+    if (x == 65 || y == 65||(x + y)  == 65){
+        return true;
+    }
+    else{
+        return false;
+    }
+  }
+  
+  console.log(numbers(65,50));
+  console.log(numbers(30,6));
+  
+  
