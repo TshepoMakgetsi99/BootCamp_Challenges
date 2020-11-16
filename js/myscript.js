d08a65ba62e9ac9d69d2b9e63dd627f8f509d7cd
unknown <tshepomakgetsi@gmail.com>
2020-11-16 09:31:21 +0200

    TaskTwo

---
+++ js/myscript.js
@@ -0,0 +1,21 @@
+// TASK 2 Question........................................................................
+
+// x = 1 + 1 * 2
+// y = (1 + 1) * 2
+// z = 1 + ( 1 * 2 )
+// a =  1 + 1 * 2 / 2
+// b =  (1 + 1 * 2 ) /  2
+
+
+
+let ltx = 1 + 1 * 2;
+let lty = (1 + 1) * 2;
+let ltz = 1 + ( 1 * 2 );
+let lta =  1 + 1 * 2 / 2;
+let ltb =  (1 + 1 * 2 ) /  2;
+
+console.log(ltx);
+console.log(lty);
+console.log(ltz);
+console.log(lta);
+console.log(ltb);
