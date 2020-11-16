5f87f108da42f19615bdf60ae0c9c50cefb70383
unknown <tshepomakgetsi@gmail.com>
2020-11-16 09:51:35 +0200

    TaskSix

---
+++ js/myscript.js
@@ -0,0 +1,15 @@
+
+    // TASK 6
+    // White a function that takes in three numbers and returns the maximum number. Do this without using any builtin methods. Write your own logic from scratch.
+
+        // Here we say return rather than console.log because we do not just want the value
+        // to be displayed we want to store it so that we can use it later.
+    // So now when we call this it won’t display on the console
+
+    function max(number1, number2, number3) {
+      
+        return Math.max(number1, number2, number3);
+       }
+    
+        console.log(max(2,45,3));
+    
