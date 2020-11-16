7f2c66a93cc20fe0db91ea7674db9d952b2753c5
unknown <tshepomakgetsi@gmail.com>
2020-11-16 10:05:03 +0200

    TaskTen

---
+++ js/myscript.js
@@ -0,0 +1,11 @@
+  // Task 10
+    // Write a function that takes in a string and then prints out all the vowels in the string. Make sure it can deal with capital letters and small letters.
+    function getVowels(word) {
+
+        let vowels = /[aeiouAEIOU]/g;
+    //global search with regular expression for vowels
+        console.log(word.match(vowels));
+        }
+    
+    
+        getVowels('TshepoEnestinahMakgetsi');
