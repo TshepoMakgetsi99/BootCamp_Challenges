1afb4e7b1fe67e20f2754ac0bdea7fe94462fa9c
unknown <tshepomakgetsi@gmail.com>
2020-11-16 09:48:07 +0200

    TaskFive

---
+++ js/myscript.js
@@ -0,0 +1,11 @@
+// Task 5
+// Write a function that takes in three numbers. These numbers represent the lengths of the sides of a triangle. The function should return the area of a triangle.
+function TriangleArea(side1, side2, side3) {
+
+    let s = (side1+side2+side3)/2;
+    let area = Math.sqrt(s*(s - side1)*(s-side2)*(s-side3));
+
+    console.log(area);
+
+}
+        TriangleArea(2,4,4);
