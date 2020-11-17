f61918ef308e62462324313022212863e097684c
unknown <tshepomakgetsi@gmail.com>
2020-11-17 11:17:10 +0200

    Task7 corrections

--- js/myscript.js
+++ js/myscript.js
@@ -1,19 +1,19 @@
 // Task 7
 // Write a function that takes in a number representing the temperature in Celsius and returns the temperature in Fahrenheit. Write another function that does the opposite (Fereignheit to Celsius)
-function Ftshe(celsius) 
+function cTshe(celsius) 
 {
-  let fTem = celsius;
-  let fToFahr = fTem * 9 / 5 + 32;
-  let message = fTem+'\xB0C is ' + fToFahr + ' \xB0F.';
+  const cTemp = celsius;
+  const cToFahr = cTemp * 9 / 5 + 32;
+  const message = ` ${cToFahr} \xB0F.`;
     console.log(message);
 }
 
-function fDim(fahrenheit) 
+function fTha(fahrenheit) 
 {
-  let dTem = fahrenheit;
-  let dToCel = (dTem - 32) * 5 / 9;
-  let message = dTem+'\xB0F is ' + dToCel + '\xB0C.';
+  const fTemperature = fahrenheit;
+  const fToCelsius = (fTemperature - 32) * 5 / 9;
+  const message = ` ${fToCelsius}\xB0C.`;
     console.log(message);
 } 
-Ftshe(40);
- fDim(45);
+cTshe(80);
+fTha(55);
